fd07cb7ade0c1871eea356e21e3613dcaa080a5b
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2016-04-23 14:25:35 +0100

    Remove dist from master

--- dist/js/app.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    $('.js-team-image').mouseenter(function () {
-        var imagePath = '/img/',
-            numberOfPics = 4,
-            randomPic = Math.floor(Math.random() * numberOfPics);
-        $(this).attr('src', imagePath + randomPic + '.jpg')
-    });
-})(jQuery);
