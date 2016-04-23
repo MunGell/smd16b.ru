94919a7f8783fed62e8fd27d2ee0dae05410e986
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2016-04-23 15:06:20 +0100

    Add dist directory

---
+++ dist/js/app.js
@@ -0,0 +1,8 @@
+(function ($) {
+    $('.js-team-image').mouseenter(function () {
+        var imagePath = '/img/',
+            numberOfPics = 4,
+            randomPic = Math.floor(Math.random() * numberOfPics);
+        $(this).attr('src', imagePath + randomPic + '.jpg')
+    });
+})(jQuery);
